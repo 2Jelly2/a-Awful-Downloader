7c8d83f7380319d5788b4ae0077dbaf93d11b06c
2Jelly2 <Shigure@OsanaNajimi.Moe>
2020-10-15 17:35:22 +0800

    Update AwfulDownloader.java

--- AwfulDownloader.java
+++ AwfulDownloader.java
@@ -3,6 +3,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
+//import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.net.Socket;
 //import java.net.InetAddress;
@@ -24,23 +25,51 @@ class AwfulDownloader
             InetSocketAddress socketAddr = new InetSocketAddress(url.getHost(), port);
             client.connect(socketAddr);
             */
-            
-			fileName = getFilename(url.getPath());
-			OutputStream outputStream = new FileOutputStream(fileName == null ? "output.txt" : fileName);
 			
-			long startTime = System.currentTimeMillis();
-            String request = "GET " + url.getPath() + " HTTP/1.1\r\n" + "Host: " + url.getHost() + ":" + port +"\r\n";
-            PrintWriter pWriter = new PrintWriter(client.getOutputStream(),true);
+            String request = "GET " + url.getPath() + " HTTP/1.1\r\n";
+            request += "Host: " + url.getHost() + ":" + port +"\r\n";
+            request += "Connection: close\r\n";
+            PrintWriter pWriter = new PrintWriter(client.getOutputStream(), true);
             pWriter.println(request);
-            
+
+			long startTime = System.currentTimeMillis();
 			InputStream inputStream = client.getInputStream();
-			byte[] buffer = new byte[8 * 1024];
+			OutputStream outputStream = new FileOutputStream(getFilename(url.getPath()));
+			
+			// Skip until contents
 			int bytesNum;
+			byte buffer[] = new byte[1];
+			//ArrayList<String> headers = new ArrayList<String>();
+			//String temp = "";
+			byte[] divider = {13, 10, 13, 10};
+			int counter = 0;
+			while((bytesNum = inputStream.read(buffer)) != -1)
+			{
+				if(buffer[0] == 13 || buffer[0] == 10)
+				{
+					if(buffer[0] == divider[counter])
+					{
+						counter ++;
+						if(counter == divider.length)
+						{
+							break;
+						}
+					}
+				}
+				else
+				{
+					counter = 0;
+				}
+			}
+			
+			// Output contents to a file
+			buffer = new byte[8 * 1024];
 			while((bytesNum = inputStream.read(buffer)) != -1)
 			{
 				outputStream.write(buffer, 0, bytesNum);
+				//buffer = inputStream.available() < buffer.length ? new byte[inputStream.available()] : new byte[8 * 1024];
 			}
-			outputStream.close();
+			
 			client.close();
 			System.out.println("Download Complete.\nTime Cost: " + (System.currentTimeMillis() - startTime) + "ms");
 		}
@@ -54,21 +83,22 @@ class AwfulDownloader
 	{
 		//String defaultURL = "https://mirrors.neusoft.edu.cn/eclipse/oomph/epp/2020-09/R/eclipse-inst-jre-win64.exe";
 		//String defaultURL = "https://osananajimi.moe/content/images/size/w2000/2020/07/DSC01321.jpg";
-		String defaultURL = "http://api64.ipify.org/";
+		//String defaultURL = "http://api64.ipify.org/";
+		String defaultURL = "http://lain.bgm.tv/pic/cover/l/7f/4e/209615_iAw8I.jpg";
 		AwfulDownloader pdl = new AwfulDownloader(args.length == 0 ? defaultURL : args[0]);
 	}
 	
 	private String getFilename(String path)
 	{
+		String fileName = new String();
 		StringTokenizer token = new StringTokenizer(path, "/");
 		
 		while(token.hasMoreTokens())
 		{
 			fileName = token.nextToken();
 		}
-		return fileName;
+		return fileName == null ? "output.txt" : fileName;
 	}
 
 	private String userAgent = "Awful Downloader v0.1";
-	private String fileName;
 }
